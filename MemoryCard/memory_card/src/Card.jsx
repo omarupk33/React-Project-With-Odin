693928a96dbca91f01881da571cbc38b35790292
omarupk33 <kkomar12334@gmail.com>
2026-08-14 23:31:16 +0300

    Learn how to use Events with useEffect

--- MemoryCard/memory_card/src/Card.jsx
+++ MemoryCard/memory_card/src/Card.jsx
@@ -5,24 +5,32 @@ function Card({ num, data, handleFlip, card1, card1Handle}){
         
         const [clicked, isClicked] = useState('')
         const [found, setFound] = useState(false)
-
         let image
+
         if(Object.values(data).at(0)){
           image = <img  width='100%' height='100%' src={Object.values(data).at(0).at(num).images.original.url}
           alt='gif'/>
-
-            let gifId = Object.values(data).at(0).at(num).id
-
           
-            useEffect(()=>{
-                setFound(handleFlip(num))
-            }, [clicked])
+        }
+
+        let lengthOfCardArrays
+        useEffect(()=>{
+            setFound(handleFlip(num))
+            let allClickedCards = document.querySelectorAll('.clicked')
+            lengthOfCardArrays = allClickedCards.length
+            console.log(lengthOfCardArrays)
+            
+            return ()=>{
+                allClickedCards = []
+                lengthOfCardArrays = 0
             }
+        }, [clicked])
 
 
             const focusStyle = ()=>{
+            if(lengthOfCardArrays < 2){
             if(!clicked){isClicked('clicked')}
-            else(isClicked(''))
+            else(isClicked(''))}
             }
 
 
